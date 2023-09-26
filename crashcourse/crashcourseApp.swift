7dc869dc2fbae3abdfef87b68296bb4d92a93a8c
egemen.ongel <egemenongel@gmail.com>
2023-09-26 17:07:11 +0300

    Initial Commit

---
+++ crashcourse/crashcourseApp.swift
@@ -0,0 +1,17 @@
+//
+//  crashcourseApp.swift
+//  crashcourse
+//
+//  Created by Egemen Öngel on 26.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct crashcourseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

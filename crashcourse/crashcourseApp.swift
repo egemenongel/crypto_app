cceafd0c95d5a092c35ad628e2009e5b2b9455c4
egemen.ongel <egemenongel@gmail.com>
2023-10-07 17:16:16 +0300

    fix: Update home view with nav view

--- crashcourse/crashcourseApp.swift
+++ crashcourse/crashcourseApp.swift
@@ -11,7 +11,9 @@ import SwiftUI
 struct crashcourseApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            NavigationView{
+                HomeView().toolbar(.hidden)
+            }
         }
     }
 }

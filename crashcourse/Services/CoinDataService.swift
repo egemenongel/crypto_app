b2342d910aaac1fe137ecf9fb064c9e4cf891b15
egemenongel <egemenongel@hotmail.com>
2024-05-08 16:30:30 +0300

    refactor: Refactor networking manager

--- crashcourse/Services/CoinDataService.swift
+++ crashcourse/Services/CoinDataService.swift
@@ -21,11 +21,10 @@ class CoinDataService{
         guard let url = URL(string: "https://sandbox-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?start=1&limit=100&sort=market_cap&cryptocurrency_type=all&tag=all")
         else { return }
 
-
         var request = URLRequest(url: url)
         request.setValue(String("7cee5147-c18a-4e64-8b54-13d0ba168fd4"), forHTTPHeaderField: "X-CMC_PRO_API_KEY")
 
-        coinSubscription = NetworkingManager.download(urlRequest: request)
+        coinSubscription = NetworkingManager.get(urlRequest: request)
             .decode(type: CoinsModel.self, decoder: JSONDecoder())
             .sink(receiveCompletion: NetworkingManager.handleCompletion, 
                   receiveValue: {[weak self] (returnedCoins) in
--- crashcourse/Utilities/NetworkingManager.swift
+++ crashcourse/Utilities/NetworkingManager.swift
@@ -10,36 +10,20 @@ import Combine
 
 class NetworkingManager{
 
-    enum NetworkingError: LocalizedError{
-        case badURLResponse(url: URL)
-        case unknown
-
-        var errorDescription: String?{
-            switch self {
-            case .badURLResponse(url: let url): return  "[🔥] Bad response from URL: \(url)"
-            case .unknown: return "[⚠️] Unknown error occured"
-            }
-        }
-    }
-
-     static func download(urlRequest: URLRequest)-> AnyPublisher<Data, Error> {
-        return URLSession.shared.dataTaskPublisher(for: urlRequest)
-            .subscribe(on: DispatchQueue.global(qos: .default))
-            .tryMap({try handleURLResponse(output: $0,url: urlRequest.url!)})
-            .receive(on: DispatchQueue.main)
-            .eraseToAnyPublisher()
-    }
+    static func get(urlRequest: URLRequest)-> AnyPublisher<Data, Error> {
+    return URLSession.shared.dataTaskPublisher(for: urlRequest)
+        .subscribe(on: DispatchQueue.global(qos: .default))
+        .tryMap({try handleURLResponse(output: $0,url: urlRequest.url!)})
+        .receive(on: DispatchQueue.main)
+        .eraseToAnyPublisher()
+}
     
     static func handleURLResponse(output: URLSession.DataTaskPublisher.Output, url: URL) throws -> Data{
-        let response = output.response as? HTTPURLResponse
-       if(response!.statusCode >= 200 && response!.statusCode < 300)
-       {
-           print(response.debugDescription)
-       }
-       else{
-           print(response?.statusCode)
-           throw NetworkingError.badURLResponse(url: url)
-       }
+        guard let response = output.response as? HTTPURLResponse,response.statusCode >= 200 && response.statusCode < 300
+                else
+        {
+            throw NetworkingError.badURLResponse(url: url)
+        }
        return output.data
     }
 
@@ -49,6 +33,18 @@ class NetworkingManager{
             break
         case .failure(let error):
             print(error.localizedDescription)
+        }
+    }
+
+    enum NetworkingError: LocalizedError{
+        case badURLResponse(url: URL)
+        case unknown
+
+        var errorDescription: String?{
+            switch self {
+            case .badURLResponse(url: let url): return  "[🔥] Bad response from URL: \(url)"
+            case .unknown: return "[⚠️] Unknown error occured"
+            }
         }
     }
 }
